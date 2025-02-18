94deed59d65c81407e5259fbb37f1d9e236b93ed
LeonardAbbas <lennyabbas3@gmail.com>
2025-02-18 11:49:57 -0600

    6.0

--- check_playlist.ps1
+++ check_playlist.ps1
@@ -1,26 +1,22 @@
 $videosOffline = Get-Content -Path "videos_offline.txt" -Encoding UTF8 | ForEach-Object { $_ -replace '!', '' }
 $videosOnline = Get-Content -Path "videos_online.txt" -Encoding UTF8 | ForEach-Object { $_ -replace '!', '' }
 
-$downloadedCount = 0
-$notDownloadedCount = 0
-$videoNumber = 1
-
-# Loop through each line in videosOnline.txt
-foreach ($video in $videosOnline) {
+foreach ($video in $videosOffline) {
+    $videoNumber = $video.Substring(0, 4)
+    $video = $video.Substring(9)
     $escapedVideo = [regex]::Escape($video)
-    $match = $videosOffline | Select-String -Pattern "^*mp3.$escapedVideo$"
+    $match = $videosOnline | Select-String -Pattern "^$escapedVideo$"
     
     if ($match.Count -gt 1) {
-        Write-Output "Multiple matches found for $video"
+        Write-Output "Multiple matches found for $videoNumber.mp3 $video"
         $match.LineNumber
-        $match = $match | Sort-Object { [math]::Abs([int]($_.Line.Substring(0, 4)) - $videoNumber) } | Select-Object -First 1
+        $match = $match | Sort-Object { [math]::Abs($videoNumber - $_.LineNumber) } | Select-Object -First 1
     }
 
     if ($match) {
-        $downloadedCount++
-        $videoNumberPadded = $videoNumber.ToString("D4")
+        $videoNumberPadded = $match.LineNumber.ToString("D4")
 
-        $oldName = "$($match.Line.Substring(0, 4)).mp3"
+        $oldName = "$videoNumber.mp3"
         $newName = "$videoNumberPadded.mp3"
         if ($oldName -ne $newName) {
             if (Test-Path -Path "temp\$newName") {
@@ -30,12 +26,7 @@ foreach ($video in $videosOnline) {
         }
     }
     else {
-        $notDownloadedCount++
         Write-Output "Not found: $video"
+        Remove-Item -Path "temp\$videoNumber.mp3"
     }
-
-    $videoNumber++
 }
-
-Write-Output "Downloaded: $downloadedCount"
-Write-Output "Not downloaded: $notDownloadedCount"
