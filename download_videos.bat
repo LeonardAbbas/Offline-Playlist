89c46dfb0fa5bfff77d8a911a7b60403e013f476
LeonardAbbas <lennyabbas3@gmail.com>
2025-02-19 17:52:29 -0600

    8.0

--- download_videos.bat
+++ download_videos.bat
@@ -27,4 +27,16 @@ if %songsDownloaded% lss %songsInPlaylist% (
     goto :download_loop
 )
 
+for /L %%i in (1,1,!songsInPlaylist!) do (
+    set "num=0000%%i"
+    set "num=!num:~-4!"
+    if not exist "temp\!num!.mp3" (
+        echo Downloading !num!
+        yt-dlp -I %%i
+        if %%i lss 1000 (
+            ren "temp\%%i.mp3" "!num!.mp3"
+        )
+    )
+)
+
 endlocal
--- update.bat
+++ update.bat
@@ -3,11 +3,10 @@ setlocal enabledelayedexpansion
 
 call unsort.bat
 call get_videos.bat
-call download_new_videos.bat
 
 powershell.exe -ExecutionPolicy Bypass -File check_playlist.ps1
 
-call download_missing_videos.bat
+call download_videos.bat
 
 del videos_offline.txt
 del videos_offline_channels.txt
