d962a2c7e233ed95eecb1a50695106a7f0068f01
LeonardAbbas <lennyabbas3@gmail.com>
2025-03-02 11:45:56 -0600

    12.0

--- update.bat
+++ update.bat
@@ -3,8 +3,27 @@ setlocal enabledelayedexpansion
 
 echo Started updating: %time%
 
+start "" /B yt-dlp --flat-playlist --print title --print duration_string -I ::-1 > videos_online.txt
 call unsort.bat
-call get_videos.bat
+start "" /B exiftool -T -FileName -Duration -Title temp\*.mp3 > videos_offline.txt
+
+:: Wait for exiftool to finish
+:wait_exiftool
+tasklist /FI "IMAGENAME eq exiftool.exe" 2>NUL | find /I /N "exiftool.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+    timeout /T 1 /NOBREAK > NUL
+    goto wait_exiftool
+)
+
+:: Wait for yt-dlp to finish
+:wait_ytdlp
+tasklist /FI "IMAGENAME eq yt-dlp.exe" 2>NUL | find /I /N "yt-dlp.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+    timeout /T 1 /NOBREAK > NUL
+    goto wait_ytdlp
+)
+
+echo Starting to check playlist: %time%
 
 powershell.exe -ExecutionPolicy Bypass -File check_playlist.ps1
 
@@ -13,7 +32,7 @@ call download_videos.bat
 del videos_offline.txt
 del videos_online.txt
 
-call sort.bat
+@REM call sort.bat
 
 echo Finished updating: %time%
 
